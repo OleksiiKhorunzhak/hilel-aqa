60c5e296c603d63d118fe96b00f2d0968f0645b4
Alex <ohorunzhak@gmail.com>
2024-02-21 18:48:01 +0200

    Stage 1

---
+++ AtataUITests/AtataConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace AtataUITests
+{
+    public sealed class AtataConfig : JsonConfig<AtataConfig>
+    {
+        // Custom configuration properties can be added here.
+        // See https://github.com/atata-framework/atata-configuration-json#custom-settings
+    }
+}
