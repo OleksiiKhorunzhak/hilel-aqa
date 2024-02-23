f7570d83662e41a12602432475c6f7c31cb36ea0
Alex <ohorunzhak@gmail.com>
2024-02-23 16:23:57 +0200

    update tests

--- AtataUITests/Homework/VariablesTests.cs
+++ AtataUITests/Homework/VariablesTests.cs
@@ -6,7 +6,7 @@
         [Description("TC-1 higth expected to be bigger than low")]
         public void IntegerTest()
         {
-            //TODO: uncomment and fix code below *
+            //TODO: uncomment and fix code below 
 
             //low = 32;
             //higth = 64;
@@ -18,7 +18,7 @@
         [Description("TC-2 part expected to be smaller than whole")]
         public void DecimalTest()
         {
-            //TODO: uncomment and fix code below **
+            //TODO: uncomment and fix code below 
 
             //part = 25.5;
             //whole = 100;
@@ -26,7 +26,7 @@
             //Assert("part is not smaller than whole");
         }
 
-        //TODO: uncomment and fix code below ***
+        //TODO: uncomment and fix code below 
         //[Description("TC-3 text that represent higth should match expected pattern")]
         //public void StringsTest()
         //{
