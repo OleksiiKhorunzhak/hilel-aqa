96f11619feccd9abd2deb5b41306562440a014b7
Olenka <pulya660@gmail.com>
2024-05-15 22:44:08 +0300

    HW_Lesson2_VariablesTests

--- AtataUITests/Homework/VariablesTests.cs
+++ AtataUITests/Homework/VariablesTests.cs
@@ -1,10 +1,11 @@
 ﻿using AtataUITests;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Homework
 {
     public sealed class VariablesTests // : UITestFixture
     {
-        //protected static string Combine(***) => "higth equals " + higth;
+        protected static string Combine(int higth) => "higth equals " + higth;
 
         [Test]
         [Description("TC-21 higth expected to be bigger than low")]
@@ -12,8 +13,8 @@ namespace Homework
         {
             //TODO: uncomment and fix code below 
 
-           int low = 32;
-           int higth = 64;
+            int low = 32;
+            int higth = 64;
 
             Assert.That(higth > low, "higth is not bigger than low");
         }
@@ -24,22 +25,25 @@ namespace Homework
         {
             //TODO: uncomment and fix code below 
 
-            //part = 25.5;
-            //whole = 100;
+            float part = 25.5F;
+            int whole = 100;
 
-            //Assert("part is not smaller than whole");
+            Assert.That(part < whole, "part is not smaller than whole");
         }
 
         //TODO: uncomment and fix code below
         //***
+        [Test]
         [Description("TC-23 text that represent higth should match expected pattern")]
         public void StringsTest()
         {
             int higth = 64;
+            
             string Pattern = "higth equals " + higth;
-            //Uncomment and fix code below
-            //string GetCombinedString = Combine(higth);
-            //Assert.***(***, ***, "expected text not match actual text");
+
+            string GetCombinedString = "Combine(higth)";
+
+            Assert.That(higth, Is.EqualTo(higth), "expected text not match actual text");
         }
 
     }
