97e43641dfc6d22b6700256289f9741f851a8d66
Alex <ohorunzhak@gmail.com>
2024-05-24 06:30:23 +0300

    Update lesson 7

---
+++ AtataUITests/Tests/TextBoxTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtataUITests.Tests
+{
+    internal class TextBoxTests : UITestFixture
+    {
+        //Preconditions: Go to https://demoqa.com/text-box
+        //Test Case 1: Text Full Name should be visible
+        //Test Case 2: Text Full Name Input should be visible
+        //Test Case 3: Enter "John Doe" in Text Full Name Input, press submit, text Name should be "Name:John Doe"
+        //Test Case 4: Clear Text Full Name Input, press submit, text Name should not be visible
+    }
+}
