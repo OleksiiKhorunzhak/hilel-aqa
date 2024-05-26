39a188ec8ef3d8224f60ca88a8e6559f149762fc
vakar <vakarmak@gmail.com>
2024-05-26 15:30:21 +0300

    Text box test

--- AtataUITests/Tests/TextBoxTests.cs
+++ AtataUITests/Tests/TextBoxTests.cs
@@ -13,5 +13,20 @@ namespace AtataUITests.Tests
         //Test Case 2: Text Full Name Input should be visible
         //Test Case 3: Enter "John Doe" in Text Full Name Input, press submit, text Name should be "Name:John Doe"
         //Test Case 4: Clear Text Full Name Input, press submit, text Name should not be visible
+        //Test Case 5: Text Email should be visible
+        //Test Case 6: Text Email Input should be visible
+        //Test Case 7: Enter valid email in Text Email Input, press submit, text Email should be "Email:valid email"
+        //Test Case 8: Clear Text Email Input, press submit, text Email should hide
+        //Test Case 9: Enter invalid email in Text Email Input, press submit, email input framed with red color
+        //Test Case 10: Text Current Address should be visible
+        //Test Case 11: Text Current Address Input should be visible
+        //Test Case 12: Current Address Input area is resizable
+        //Test Case 13: Enter "Current Address" in Text Current Address Input, press submit, text Current Address should be "Current Address:Current Address"
+        //Test Case 14: Clear Text Current Address Input, press submit, text Current Address should be visible, Current Address Input should be empty
+        //Test Case 15: Text Permanent Address should be visible
+        //Test Case 16: Text Permanent Address Input should be visible
+        //Test Case 17: Permanent Address Input area is resizable
+        //Test Case 18: Enter "Permanent Address" in Text Permanent Address Input, press submit, text Permanent Address should be "Permanent Address:Permanent Address"
+        //Test Case 19: Clear Text Permanent Address Input, press submit, text Permanent Address should not be visible, Permanent Address Input should be empty
     }
 }
