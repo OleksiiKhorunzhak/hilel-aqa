85b3077ca12997cfea24c04f2cce0f43484802b0
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-05-30 14:40:55 +0300

    homework lesson 7

--- PlaywrigthUITests/UITestFixture.cs
+++ PlaywrigthUITests/UITestFixture.cs
@@ -15,11 +15,17 @@ namespace PlaywrigthUITests
             var playwrightDriver = await Playwright.CreateAsync();
             browser = await playwrightDriver.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                Headless = false // Set to false to run the browser in non-headless mode
+                Headless = false, // Set to false to run the browser in non-headless mode
+                Args = new List<string> { "--start-maximized" }
             });
-            var context = await browser.NewContextAsync();
-            Page = await context.NewPageAsync();
+            var context = await browser.NewContextAsync(new BrowserNewContextOptions
+			{
+				ViewportSize = ViewportSize.NoViewport
+			});
+
+			Page = await context.NewPageAsync();
         }
+
 
         [TearDown]
         public async Task Teardown()
