8c1f582a924fcdc2193a4b3e37c6a9b3d8cce690
Alex <ohorunzhak@gmail.com>
2024-05-30 17:30:33 +0300

    Add lesson 9

--- AtataUITests/PageObjects/DemoQATextBoxPage.cs
+++ AtataUITests/PageObjects/DemoQATextBoxPage.cs
@@ -11,6 +11,7 @@ namespace AtataUITests.PageObjects
         [FindById("userName-label")]
         public Label<_> FullNameLabel { get; set; }
 
+        [WaitSeconds(1, TriggerEvents.BeforeClick)]
         public Button<_> Submit { get; set; }
 
         [FindById("name")]
