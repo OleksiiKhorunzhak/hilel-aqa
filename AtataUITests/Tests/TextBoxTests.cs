a0020f2675c2f38fd98983160c04e2a03e23b27a
kristinapasichna <92912382+kristinapasichna@users.noreply.github.com>
2024-06-01 11:01:14 +0300

    resolve merge conflicts

---
+++ AtataUITests/Tests/TextBoxTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtataUITests.Tests
+{
+    internal class TextBoxTests : UITestFixture
+    {
+        //Preconditions: Go to https://demoqa.com/text-box
+        //Test Case 1: Text Full Name should be visible
+        //Test Case 2: Text Full Name Input should be visible
+        //Test Case 3: Enter "John Doe" in Text Full Name Input, press submit, text Name should be "Name:John Doe"
+        //Test Case 4: Clear Text Full Name Input, press submit, text Name should not be visible
+        //Test Case 5: Text Email should be visible
+        //Test Case 6: Text Email Input should be visible
+        //Test Case 7: Enter "myemail@myemail.com" in the Email input, press submit, text Email should be "Email:myemail@myemail.com"
+        //Test Case 8: Enter "1" in the Email input, press submit, Email input should be "Email:1" and the borders highlighted in red
+        //Test Case 9: Enter "myemail@myemail" in the Email input, press submit, Email input should be "Email:myemail@myemail" and the borders highlighted in red
+        //Test Case 10: Enter "myemail@myemail." in the Email input, press submit, Email input should be "Email:myemail@myemail." and the borders highlighted in red
+        //Test Case 11: Enter "myemail@myemail.c" in the Email input, press submit, Email input should be "Email:myemail@myemail.c" and the borders highlighted in red
+        //Test Case 12: Enter "@myemail.com" in the Email input, press submit, Email input should be "Email:@myemail.com" and the borders highlighted in red
+        //Test Case 13: Enter "myemail.com" in the Email input, press submit, Email input should be "Email:myemail.com" and the borders highlighted in red
+        //Test Case 14: Enter "current address" in the Current Address input, press submit, text Current Address should be "Current Address : current address"
+        //Test Case 15: Enter "1111" in the Current Address input, press submit, text Current Address should be "Current Address : 1111"
+        //Test Case 16: Enter "permanent address" in the Permanent Address input, press submit, text Permanent Address should be "Permanent Address : permanent address"
+        //Test Case 17: Enter "2222" in the Permanent Address input, press submit, text Permanent Address should be "Permanent Address : 2222"
+        
+    }
+}
