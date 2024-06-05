953340af824f79b0a5a1ffb96352916fd15c737e
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-05 15:03:47 +0300

    PW RadioButton test 3

--- PlaywrigthUITests/PageObjects/DemoQARadioButtonPage.cs
+++ PlaywrigthUITests/PageObjects/DemoQARadioButtonPage.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Playwright;
+﻿using Atata;
+using Microsoft.Playwright;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,24 @@ namespace PlaywrigthUITests.PageObjects
         {
             _page = page;
         }
+
+        public async Task GoToRadioButtonPage()
+        {
+            await _page.GotoAsync(elementsPageUrl);
+            await _page.GetByText("Elements").ClickAsync();
+            await _page.GetByText("Radio Button").ClickAsync();
+            await _page.WaitForURLAsync(radioButtonPageUrl);
+        }
+
+        public async Task CheckYesRadioButton()
+        {
+            await _page.GetByText("Yes").CheckAsync();
+            //await _page.Locator("label").Filter(new() { HasText = "Yes" }).CheckAsync();
+        }
+        public async Task VerifyTextYesVisible()
+        {
+            await Assertions.Expect(_page.GetByText("You have selected Yes")).ToBeVisibleAsync();
+        }
+
     }
 }
--- PlaywrigthUITests/Tests/RadioButtonsTests.cs
+++ PlaywrigthUITests/Tests/RadioButtonsTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Atata;
+using PlaywrigthUITests.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,21 @@ namespace PlaywrigthUITests.Tests
 {
     internal class RadioButtonsTests : UITestFixture
     {
+        private DemoQARadioButtonPage _radioButtonPage;
 
+        [SetUp]
+        public void SetupRadioButtonPage()
+        {
+            _radioButtonPage = new DemoQARadioButtonPage(Page);
+        }
+
+        [Test]
+        [Description("Check 'Yes' radiobutton")]
+        public async Task VerifyYesRadioButtn()
+        {
+            await _radioButtonPage.GoToRadioButtonPage();
+            await _radioButtonPage.CheckYesRadioButton();
+            await _radioButtonPage.VerifyTextYesVisible();
+        }
     }
 }
