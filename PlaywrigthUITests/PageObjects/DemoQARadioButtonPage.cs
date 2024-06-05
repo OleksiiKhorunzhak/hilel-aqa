c3950c2fa8408594a8fc28e3847f58ecb03309cc
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-05 12:17:24 +0300

    RadioButtonPageSetup

---
+++ PlaywrigthUITests/PageObjects/DemoQARadioButtonPage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrigthUITests.PageObjects
+{
+    internal class DemoQARadioButtonPage
+    {
+        private IPage _page;
+        private string elementsPageUrl = "https://demoqa.com/elements";
+        private string radioButtonPageUrl = "https://demoqa.com/radio-button";
+
+        public DemoQARadioButtonPage(IPage page)
+        {
+            _page = page;
+        }
+    }
+}
