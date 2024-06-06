1a6d6a21d09080f904b0bce49044e563f2a13e3b
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-06-06 11:33:28 +0300

    homework checkbox page tests

---
+++ PlaywrigthUITests/PageObjects/DemoQACheckBoxPage.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrigthUITests.PageObjects
+{
+	internal class DemoQACheckBoxPage
+	{
+		private IPage _page;
+		private string elementsPageUrl = "https://demoqa.com/elements";
+		private string CheckBoxPageUrl = "https://demoqa.com/checkbox";
+
+		public DemoQACheckBoxPage(IPage page)
+		{
+			_page = page;
+		}
+
+		#region Actions
+		public async Task GoToCheckBoxPage()
+		{
+			await _page.GotoAsync(elementsPageUrl);
+			await _page.GetByText("Check Box").ClickAsync();
+			await _page.WaitForURLAsync(CheckBoxPageUrl);
+		}
+
+		public async Task ClickExpandAllButton()
+		{
+			await _page.GetByLabel("Expand all").ClickAsync();
+		}
+
+		public async Task ClickCollapseAllButton()
+		{
+			await _page.GetByLabel("Collapse all").ClickAsync();
+		}
+
+		public async Task ClickCheckBoxByName(string name)
+		{
+			await _page.GetByText($"{name}").ClickAsync();
+		}
+
+		public async Task ClickHomeToggle()
+		{
+			await _page.GetByLabel("Toggle").ClickAsync();
+		}
+
+		public async Task ClickToggleByDirectoryName(string name)
+		{
+			await _page.Locator("li").Filter(new() { HasTextRegex = new Regex($"^{name}$") }).GetByLabel("Toggle").ClickAsync();
+		}
+
+		#endregion
+
+		#region Verifications
+		public async Task<bool> CheckCheckBoxHeaderVisibility()
+		{
+			return await _page.GetByRole(AriaRole.Heading, new() { Name = "Check Box" }).IsVisibleAsync();
+		}
+
+		public async Task CheckCheckBoxHeaderIsVisible()
+		{
+			await Assertions.Expect(_page.GetByRole(AriaRole.Heading, new() { Name = "Check Box" })).ToBeVisibleAsync();
+		}
+
+		public async Task VerifyCheckBoxByName(string name)
+		{
+			await _page.GetByText($"{name}").IsCheckedAsync();
+		}
+
+		public async Task VerifyCheckBoxNotVisibleByName(string name)
+		{
+			await Assertions.Expect(_page.GetByText($"{name}")).Not.ToBeVisibleAsync();
+		}
+
+		public async Task VerifyCheckBoxIsNotSelectedByLabelText(string labelText)
+		{
+			await Assertions.Expect(_page.Locator("label:has(svg.rct-icon.rct-icon-uncheck)").Filter(new() { HasText = $"{labelText}" })).ToBeVisibleAsync();
+		}
+
+		public async Task VerifyCheckBoxIsSelectedByLabelText(string labelText)
+		{
+			await Assertions.Expect(_page.Locator("label:has(svg.rct-icon.rct-icon-check)").Filter(new() { HasText = $"{labelText}" })).ToBeVisibleAsync();
+		}
+
+		public async Task CheckYouHaveSelectedRowWithFileName(string fileName)
+		{
+			await Assertions.Expect(_page.GetByText($"You have selected :{fileName}")).ToBeVisibleAsync();
+		}
+
+		public async Task CheckYouHaveSelectedRowIsNotVisible()
+		{
+			await Assertions.Expect(_page.GetByText($"You have selected :")).Not.ToBeVisibleAsync();
+		}
+		#endregion
+		//await page.GetByLabel("Expand all").ClickAsync();
+		//await page.GetByText("Excel File.doc").ClickAsync();
+		//await page.GetByText("You have selected :").ClickAsync();
+		//
+		//
+		//await page.Locator("label").Filter(new () { HasText = "Desktop" }).GetByRole(AriaRole.Img).First.ClickAsync();
+		//await page.Locator("label").Filter(new() { HasText = "Notes" }).GetByRole(AriaRole.Img).First.ClickAsync();
+		//await page.Locator("label").Filter(new() { HasText = "Notes" }).GetByRole(AriaRole.Img).First.ClickAsync();
+
+		//		rct-icon.rct-icon-uncheck
+		//		rct-icon.rct-icon-check
+		//		await page.GetByLabel("Toggle").First.ClickAsync();
+		//await page.Locator("span").Filter(new () { HasText = "Home" }).First.ClickAsync();
+		//await page.Locator("#tree-node span").Nth(1).ClickAsync();
+		//	await page.Locator("#result").ClickAsync();
+	}
+}
---
+++ PlaywrigthUITests/Tests/CheckBoxTests.cs
@@ -0,0 +1,144 @@
+﻿using PlaywrigthUITests.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrigthUITests.Tests
+{
+	internal class CheckBoxTests : UITestFixture
+	{
+		private DemoQACheckBoxPage _checkboxPage;
+
+		[SetUp]
+		public void SetupCheckBoxPage()
+		{
+			_checkboxPage = new DemoQACheckBoxPage(Page);
+		}
+
+		[Test]
+		[Description("Verify header with 'Check Box' text is visible on the page")]
+		public async Task VerifyCheckBoxHeaderVisibility()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			var result = await _checkboxPage.CheckCheckBoxHeaderVisibility();
+			Assert.That(result == true, "Header 'Check Box' is not visible on the page");
+		}
+
+		[Test]
+		[Description("Verify header with 'Check Box' text is visible on the page")]
+		public async Task VerifyCheckBoxHeaderIsVisible()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.CheckCheckBoxHeaderIsVisible();
+		}
+
+		[Test]
+		[Description("Verify Expand All button expands all directories and files")]
+		public async Task VerifyExpandAllButtonFunctionality()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickExpandAllButton();
+			await _checkboxPage.VerifyCheckBoxByName("Downloads");
+			await _checkboxPage.VerifyCheckBoxByName("Excel File.doc");
+		}
+
+		[Test]
+		[Description("Verify Collapse All button collapse all directories and files")]
+		public async Task VerifyCollapseAllButtonFunctionality()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickExpandAllButton();
+			await _checkboxPage.VerifyCheckBoxByName("Excel File.doc");
+			await _checkboxPage.ClickCollapseAllButton();
+			await _checkboxPage.VerifyCheckBoxNotVisibleByName("Excel File.doc");
+		}
+
+		[Test]
+		[Description("Verify User can click on toggle and it expand lower level of files hierarchy")]
+		public async Task VerifyToggleExpandFunctionality()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickHomeToggle();
+			await _checkboxPage.VerifyCheckBoxByName("Downloads");
+			await _checkboxPage.ClickToggleByDirectoryName("Downloads");
+			await _checkboxPage.VerifyCheckBoxByName("Excel File.doc");
+		}
+
+		[Test]
+		[Description("Verify checkboxes are not selected by default")]
+		public async Task VerifyCheckBoxesAreNotSelectedByDefault()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickHomeToggle();
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Downloads");
+			await _checkboxPage.ClickToggleByDirectoryName("Downloads");
+
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Word File.doc");
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Excel File.doc");
+		}
+
+		[Test]
+		[Description("Verify checkbox become selected after click on file and unselected after re-click")]
+		public async Task VerifyCheckBoxSelectionFileFunction()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickHomeToggle();
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Downloads");
+			await _checkboxPage.ClickToggleByDirectoryName("Downloads");
+			await _checkboxPage.ClickCheckBoxByName("Excel File.doc");
+
+			await _checkboxPage.VerifyCheckBoxIsSelectedByLabelText("Excel File.doc");
+			await _checkboxPage.ClickCheckBoxByName("Excel File.doc");
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Excel File.doc");
+		}
+
+		[Test]
+		[Description("Verify checkboxes become selected after click on parent folder")]
+		public async Task VerifyCheckBoxesSelectionFunction()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickHomeToggle();
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Downloads");
+			await _checkboxPage.ClickToggleByDirectoryName("Downloads");
+			await _checkboxPage.ClickCheckBoxByName("Downloads");
+
+			await _checkboxPage.VerifyCheckBoxIsSelectedByLabelText("Downloads");
+			await _checkboxPage.VerifyCheckBoxIsSelectedByLabelText("Word File.doc");
+			await _checkboxPage.VerifyCheckBoxIsSelectedByLabelText("Excel File.doc");
+		}
+
+		[Test]
+		[Description("Verify 'You have selected' row become visible after click on checkbox and not visible after re-click")]
+		public async Task VerifyYouHaveSelectedRowAfterCheckboxClick()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickHomeToggle();
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Downloads");
+			await _checkboxPage.ClickToggleByDirectoryName("Downloads");
+			await _checkboxPage.ClickCheckBoxByName("Excel File.doc");
+
+			await _checkboxPage.CheckYouHaveSelectedRowWithFileName("excelFile");
+			await _checkboxPage.ClickCheckBoxByName("Excel File.doc");
+			await _checkboxPage.CheckYouHaveSelectedRowIsNotVisible();
+		}
+
+		[Test]
+		[Description("Verify 'You have selected' row become visible after multiple click on checkbox and not visible after re-click")]
+		public async Task VerifyMultipleFilesYouHaveSelectedRow()
+		{
+			await _checkboxPage.GoToCheckBoxPage();
+			await _checkboxPage.ClickHomeToggle();
+			await _checkboxPage.VerifyCheckBoxIsNotSelectedByLabelText("Downloads");
+			await _checkboxPage.ClickToggleByDirectoryName("Downloads");
+			await _checkboxPage.ClickCheckBoxByName("Excel File.doc");
+			await _checkboxPage.ClickCheckBoxByName("Word File.doc");
+
+			await _checkboxPage.CheckYouHaveSelectedRowWithFileName("downloadswordFileexcelFile");
+			await _checkboxPage.ClickCheckBoxByName("Excel File.doc");
+			await _checkboxPage.ClickCheckBoxByName("Word File.doc");
+			await _checkboxPage.CheckYouHaveSelectedRowIsNotVisible();
+		}
+	}
+}
