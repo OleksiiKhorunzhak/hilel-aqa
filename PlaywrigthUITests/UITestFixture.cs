d178927756395f6a2fe8e4b09a924c20671f81e2
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-06 20:23:27 +0300

    start maxi

--- PlaywrigthUITests/UITestFixture.cs
+++ PlaywrigthUITests/UITestFixture.cs
@@ -15,7 +15,8 @@ namespace PlaywrigthUITests
             var playwrightDriver = await Playwright.CreateAsync();
             browser = await playwrightDriver.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                Headless = false // Set to false to run the browser in non-headless mode
+                Headless = false, // Set to false to run the browser in non-headless mode
+                Args = new[] { "--start-maximized" } // Launch the browser maximized
             });
             var context = await browser.NewContextAsync();
             Page = await context.NewPageAsync();
