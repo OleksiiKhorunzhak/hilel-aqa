5d83900eb24639c9cc531d43a8fc46b7c2fbf8b8
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-11 13:25:05 +0300

    ViewportSize setup

--- PlaywrigthUITests/UITestFixture.cs
+++ PlaywrigthUITests/UITestFixture.cs
@@ -16,9 +16,15 @@ namespace PlaywrigthUITests
             browser = await playwrightDriver.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
                 Headless = false, // Set to false to run the browser in non-headless mode
-                Args = new List<string> { "--start-maximized", "--window-size=1920,1080" }
+                Channel = "chrome",
+                Args = new List<string> { "--start-maximized", "--window-position=0,0", /*"--window-size=1920,1080"*/ }
             });
-            var context = await browser.NewContextAsync();
+            var context = await browser.NewContextAsync(new BrowserNewContextOptions
+            {
+                ViewportSize = new ViewportSize { Width = 1900, Height = 970 },
+                // Alternatively, you can fetch the actual screen size dynamically.
+                // However, in Playwright, the viewport size setting is more reliable.
+            });
             Page = await context.NewPageAsync();
         }
 
