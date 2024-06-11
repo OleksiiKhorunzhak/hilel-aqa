c4060913a0ab835817d82ecff93d9e4f1fcd206d
Valeriia Naida <valeriia.naida@orbussoftware.com>
2024-06-11 08:51:18 +0100

    the tests were added for Checkbox page

--- PlaywrigthUITests/PageObjects/DemoQACheckBoxPage.cs
+++ PlaywrigthUITests/PageObjects/DemoQACheckBoxPage.cs
@@ -27,7 +27,7 @@ namespace PlaywrigthUITests.PageObjects
             await _page.Locator("#tree-node").GetByRole(AriaRole.Img).Nth(3).ClickAsync();
         }
 
-        public async Task CheckCheckbox(string branch)
+        public async Task ClickCheckbox(string branch)
         {
             await _page.Locator("label").Filter(new() { HasText = branch }).GetByRole(AriaRole.Img).First.ClickAsync();
         }
@@ -35,6 +35,16 @@ namespace PlaywrigthUITests.PageObjects
         public async Task OpenHome()
         {
             await _page.GetByLabel("Toggle").First.ClickAsync();
+        }
+
+        public async Task ClickToggleByName(string name)
+        {
+            await _page.Locator("li").Filter(new() { HasTextRegex = new Regex($"^{name}$") }).GetByLabel("Toggle").ClickAsync();
+        }
+
+        public async Task ExpandDocumentsTree()
+        {
+            await _page.Locator("li").Filter(new() { HasTextRegex = new Regex("^Documents$") }).GetByLabel("Toggle").ClickAsync();
         }
 
         public async Task VerifyHomeChecked()
@@ -52,5 +62,34 @@ namespace PlaywrigthUITests.PageObjects
             var checkboxVisible = await checkboxLocator.IsVisibleAsync();
             return checkboxVisible;
         }
+
+        public async Task VerifyTextAfterClickOnCheckBox(string branch)
+        {
+            await _page.GetByText("You have selected :\r\ndocuments\r\nworkspace\r\nreact\r\nangular\r\nveu\r\noffice\r\npublic\r\nprivate\r\nclassified\r\ngeneral").IsVisibleAsync();
+        }
+
+        public async Task VerifyCheckBoxHasCloseIcon(string branch)
+        {
+            //Create a locator for the React checkbox's icon
+            var iconLocator = _page.Locator($"span.rct-text:has(span.rct-title:text-is('{branch}')) svg.rct-icon.rct-icon-leaf-close");
+
+            // Use Assert to check that the close icon is visible
+            var isCloseIconVisible = await iconLocator.IsVisibleAsync();
+            Assert.That(isCloseIconVisible, Is.True, $"The checkbox for '{branch}' should have the 'rct-icon-leaf-close' icon visible.");
+        }
+
+        //public async Task<bool> VerifyCheckBoxHasCloseIcon_2(string branch)
+        //{
+        // Locate `span` element containing the branch title
+        // var spanElement = _page.Locator($"span.rct-text:has(span.rct-title:text-is('{branch}'))");
+
+        // Locator for the React checkbox's icon
+        //var closeIconIsVisible = spanElement.Locator(".rct-icon rct-icon-leaf-close");
+
+        // Check if the checkbox is visible
+        // var closeIsVisible = await closeIconIsVisible.IsVisibleAsync();
+        //return closeIsVisible;
+        //}
+
+        }
     }
-}
--- PlaywrigthUITests/Tests/CheckBoxTreeTests.cs
+++ PlaywrigthUITests/Tests/CheckBoxTreeTests.cs
@@ -1,4 +1,5 @@
-﻿using PlaywrigthUITests.PageObjects;
+﻿using Microsoft.Playwright;
+using PlaywrigthUITests.PageObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +31,7 @@ namespace PlaywrigthUITests.Tests
         {
             await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
             await _demoQACheckBoxPage.OpenHome();
-            await _demoQACheckBoxPage.CheckCheckbox("Documents");
+            await _demoQACheckBoxPage.ClickCheckbox("Documents");
             await _demoQACheckBoxPage.VerifyCheckboxChecked("Documents");
             await _demoQACheckBoxPage.VerifyCheckboxChecked("Desktop");
         }
@@ -40,7 +41,7 @@ namespace PlaywrigthUITests.Tests
         {
             await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
             await _demoQACheckBoxPage.OpenHome();
-            await _demoQACheckBoxPage.CheckCheckbox("Documents");
+            await _demoQACheckBoxPage.ClickCheckbox("Documents");
             // This should pass
             var documentsCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Documents");
             Assert.That(documentsCheck, "Documents checkbox unchecked");
@@ -50,11 +51,80 @@ namespace PlaywrigthUITests.Tests
             Assert.That(desktopCheck, "Desktop checkbox unchecked");
         }
 
-        //TODO: automate test cases
+        //TODO: Homework lesson_10 Check_box
+
         //TC4 - Check Descktop Checkbox, verify checked
-        //TC5 - Expand Home > Documents, Check Documents Checkbox. Verify WorkSpace checked
-        //TC6 - Check Documents. Verify text 'You have selected : documents workspace react angular veu office public private classified general'
-        //TC7 - Expand Home > Documents > WorkSpace, verify React have rct-icon-leaf-close icon
-        //TC8 - Check Home, Expand Home, verify Desktop, Documents, Downloads checkboxex checked
+        [Test, Description("Check Descktop Checkbox, verify checked")]
+        public async Task VerifyThatDescktopCheckboxIsChecked()
+        {
+            await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
+            await _demoQACheckBoxPage.OpenHome();
+            await _demoQACheckBoxPage.ClickCheckbox("Desktop");
+
+            var desktopCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Desktop");
+            Assert.That(desktopCheck, "Desktop checkbox is checked");
+        }
+
+        [Test, Description("Expand Home > Documents, Check Documents Checkbox. Verify WorkSpace checked")]
+        public async Task VerifyThatDocumnetsAndWorkSpaceCheckboxIsChecked()
+        {
+            await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
+            await _demoQACheckBoxPage.OpenHome();
+            await _demoQACheckBoxPage.ClickCheckbox("Documents");
+
+            var documentsCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Documents");
+            Assert.That(documentsCheck);
+
+            await _demoQACheckBoxPage.ExpandDocumentsTree();
+
+            var workspaceCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("WorkSpace");
+            Assert.That(workspaceCheck);
+        }
+
+        [Test, Description ("Check Documents. Verify text 'You have selected : documents workspace react angular veu office public private classified general")]
+        public async Task VerifyTextWhenClickOnDocumentsCheckbox()
+        {
+            await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
+            await _demoQACheckBoxPage.OpenHome();
+            await _demoQACheckBoxPage.ClickCheckbox("Documents");
+            await _demoQACheckBoxPage.VerifyTextAfterClickOnCheckBox("Documents");
+        }
+
+        [Test, Description("Expand Home > Documents > WorkSpace, verify React have rct-icon-leaf-close icon")]
+        public async Task VerifyReactElementHasCloseIcon()
+        {
+            await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
+            await _demoQACheckBoxPage.OpenHome();
+            await _demoQACheckBoxPage.ClickCheckbox("Documents");
+
+            var documentsCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Documents");
+            Assert.That(documentsCheck);
+
+            await _demoQACheckBoxPage.ExpandDocumentsTree();
+            await _demoQACheckBoxPage.ClickToggleByName("WorkSpace");
+
+            ///await _demoQACheckBoxPage.ClickCheckbox("React");
+            await _demoQACheckBoxPage.VerifyCheckBoxHasCloseIcon("React");
+        }
+
+        [Test, Description("Check Home, Expand Home, verify Desktop, Documents, Downloads checkboxes checked")]
+        public async Task VerifyThatDesktopDocumentsDownloadsCheckboxesChecked()
+        {
+            await _demoQACheckBoxPage.GoToDemoQaChecboxPage();
+            await _demoQACheckBoxPage.ClickCheckbox("Home");
+            await _demoQACheckBoxPage.OpenHome();
+
+            var desktopCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Desktop");
+            Assert.That(desktopCheck);
+
+            var documentsCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Documents");
+            Assert.That(documentsCheck);
+
+            var downloadsCheck = await _demoQACheckBoxPage.VerifyCheckboxChecked("Downloads");
+            Assert.That(downloadsCheck);
+
+        }
+
+
     }
 }
