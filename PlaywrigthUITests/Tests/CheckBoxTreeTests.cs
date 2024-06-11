bb9a9ae507643ecbd6b3e4a554a01ae11876fff9
Maksym Vakarchuk <maksym.vakarchuk@dataart.com>
2024-06-11 23:38:04 +0300

    delete unused usings

--- PlaywrigthUITests/Tests/CheckBoxTreeTests.cs
+++ PlaywrigthUITests/Tests/CheckBoxTreeTests.cs
@@ -1,9 +1,4 @@
 ﻿using PlaywrigthUITests.PageObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlaywrigthUITests.Tests
 {
