4b55e3eb3df69d644cbb9186397701e52304eb2d
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-06-13 15:25:13 +0300

    save

--- PlaywrigthUITests/Tests/TextBoxTests.cs
+++ PlaywrigthUITests/Tests/TextBoxTests.cs
@@ -18,7 +18,6 @@ namespace PlaywrigthUITests.Tests
         [Description("Text 'Full Name' should be visible")]
         public async Task VerifyTextFullName()
         {
-            //await _TextBoxPage.GoToTextBoxPage();
             var isVisible = await _TextBoxPage.IsFullNameTextlVisible();
             Assert.That(isVisible, Is.True, "The element with text 'Full Name' is not visible.");
         }
@@ -27,7 +26,6 @@ namespace PlaywrigthUITests.Tests
         [Description("FullName Placeholder should be visible")]
         public async Task VerifyFullNamePlaceholder()
         {
-            //await _TextBoxPage.GoToTextBoxPage();
             var isVisible = await _TextBoxPage.IsFullNamePlaceholderVisible();
             Assert.That(isVisible, Is.True, "The element with placeholder 'Full Name' should be visible.");
         }
@@ -37,7 +35,6 @@ namespace PlaywrigthUITests.Tests
         [Description("Enter 'Test Name 123' in Text Full Name Input, press submit, text Name should be 'Name:John Doe'")]
         public async Task VerifyFilledTextFullName()
         {
-            //await _TextBoxPage.GoToTextBoxPage();
             await _TextBoxPage.FillFullName("Test Name 123");
             await _TextBoxPage.isFullNameFocused();
             await _TextBoxPage.ClickSubmitButton();
@@ -51,7 +48,6 @@ namespace PlaywrigthUITests.Tests
         [Description("Clear Text Full Name Input, press submit, text Name should not be visible")]
         public async Task VerifyTextClearFullName()
         {
-            //await _TextBoxPage.GoToTextBoxPage();
             await _TextBoxPage.ClearFullNameInput();
             await _TextBoxPage.ClickSubmitButton();
             await _TextBoxPage.isSubmitButtonFocused();
