5b4b1777241d3af90088a7ff6b50af467071e435
Alex <ohorunzhak@gmail.com>
2024-06-20 11:21:17 +0300

    update playwrigth tests

---
+++ PlaywrigthUITests/PageObjects/DemoQADynamicPropertiesPage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Playwright;
+using System.Drawing;
+
+namespace PlaywrigthUITests.PageObjects
+{
+    internal class DemoQADynamicPropertiesPage
+    {
+        private IPage Page;
+        private string DynamicPropertiesPageUrl = "https://demoqa.com/dynamic-properties";
+
+        public DemoQADynamicPropertiesPage(IPage page)
+        {
+            this.Page = page;
+        }
+
+        public async Task GoToDemoQaDynamicPropertiesPage()
+        {
+            await Page.GotoAsync(DynamicPropertiesPageUrl);
+        }
+
+        public async Task GetColorChangeColor(string expectedColor)
+        {
+            var colorElement = Page.Locator("#colorChange");
+            var color = await colorElement.EvaluateAsync<string>("element => getComputedStyle(element).color");
+
+            Assert.That(color, Is.EqualTo(expectedColor));
+        }
+    }
+}
---
+++ PlaywrigthUITests/Tests/DynamicPropertiesTests.cs
@@ -0,0 +1,27 @@
+﻿using PlaywrigthUITests.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrigthUITests.Tests
+{
+    internal class DynamicPropertiesTests : PageTest
+    {
+        private DemoQADynamicPropertiesPage _demoQADynamicPropertiesPage;
+
+        [SetUp]
+        public void SetupDemoQAPage()
+        {
+            _demoQADynamicPropertiesPage = new DemoQADynamicPropertiesPage(Page);
+        }
+
+        [Test, Description("Verify ColorChange button have color black at page init and after 5 sec color red")]
+        public async Task VerifyDynamicColorChange()
+        {
+            await _demoQADynamicPropertiesPage.GoToDemoQaDynamicPropertiesPage();
+            await _demoQADynamicPropertiesPage.GetColorChangeColor("expectedcolor");
+        }
+    }
+}
