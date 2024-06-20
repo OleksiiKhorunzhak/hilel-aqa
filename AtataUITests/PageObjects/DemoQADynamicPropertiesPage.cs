3627d76861e7ec0d6b4945b4bf64cdcf65dd3c01
Alex <ohorunzhak@gmail.com>
2024-06-20 10:52:30 +0300

    add dynamic properties to atata proj

---
+++ AtataUITests/PageObjects/DemoQADynamicPropertiesPage.cs
@@ -0,0 +1,11 @@
+﻿using Atata;
+using _ = AtataUITests.PageObjects.DemoQADynamicPropertiesPage;
+
+namespace AtataUITests.PageObjects
+{
+    [Url("/dynamic-properties")]
+    public sealed class DemoQADynamicPropertiesPage : Page<_>
+    {
+        public Button<_> ColorChange { get; set; }
+    }
+}
---
+++ AtataUITests/Tests/DynamicPropertiesTests.cs
@@ -0,0 +1,19 @@
+﻿using AtataUITests.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtataUITests.Tests
+{
+    internal class DynamicPropertiesTests
+    {
+        [Test]
+        public void TestChangeColor()
+        {
+            Go.To<DemoQADynamicPropertiesPage>().
+                ColorChange.Css["color"].Should.WithinSeconds(2).Be("expected");
+        }
+    }
+}
