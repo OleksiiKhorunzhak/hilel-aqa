4d62b2dbb972309cf4b80684f88a9073f8890a14
Oleksii <ohorunzhak@gmail.com>
2024-06-25 18:50:50 +0300

    Add WebTableTests

---
+++ PlaywrigthUITests/PageObjects/DemoQAWebTablesPage.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrigthUITests.PageObjects
+{
+    internal class DemoQAWebTablesPage
+    {
+        private IPage _page;
+        private string WebTablePageUrl = "https://demoqa.com/webtables";
+
+        public DemoQAWebTablesPage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task GoToDemoQaWebTablesPage()
+        {
+            await _page.GotoAsync(WebTablePageUrl);
+        }
+
+        public async Task CheckTableVisible()
+        {
+            await _page.Locator("#tree-node").GetByRole(AriaRole.Img).Nth(3).ClickAsync();
+        }
+
+       
+    }
+}
---
+++ PlaywrigthUITests/Tests/WebTableTests.cs
@@ -0,0 +1,24 @@
+﻿using PlaywrigthUITests.PageObjects;
+
+namespace PlaywrigthUITests.Tests
+{
+    internal class WebTableTests : UITestFixture
+    {
+        private DemoQAWebTablesPage DemoQAWebTablesPage;
+
+        [SetUp]
+        public void SetupDemoQAPage()
+        {
+            DemoQAWebTablesPage = new DemoQAWebTablesPage(Page);
+        }
+
+        [Test]
+        public async Task VerifyCheckBoxChecked()
+        {
+            await DemoQAWebTablesPage.GoToDemoQaWebTablesPage();
+            await DemoQAWebTablesPage.CheckTableVisible();
+        }
+
+               //TODO: automate test cases
+    }
+}
