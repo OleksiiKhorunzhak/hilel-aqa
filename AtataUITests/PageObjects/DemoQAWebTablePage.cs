20aa2bb2a2c02a6f5de0e58d233ca603f4c34a90
Oleksii <ohorunzhak@gmail.com>
2024-06-25 18:38:06 +0300

    add teabe page

---
+++ AtataUITests/PageObjects/DemoQAWebTablePage.cs
@@ -0,0 +1,23 @@
+﻿using Atata;
+using AtataUITests.PageObjects.Controls;
+using _ = AtataUITests.PageObjects.DemoQAWebTablePage;
+
+namespace AtataUITests.PageObjects
+{
+    public sealed class DemoQAWebTablePage : DemoQAPage<_>
+    {
+        public ReactTable<WebTableRow, _> WebTable { get; private set; }
+
+        public class WebTableRow : TableRow<_>
+        {
+            public Text<_> FirstName { get; private set; }
+            public Text<_> LastName { get; private set; }
+            public Text<_> Age { get; private set; }
+            public Text<_> Email { get; private set; }
+            public Text<_> Salary { get; private set; }
+            public Text<_> Department { get; private set; }
+            public Link<_> Action { get; private set; }  // Assuming Action is a link
+        }
+
+    }
+}
---
+++ AtataUITests/Tests/TableTests.cs
@@ -0,0 +1,16 @@
+﻿using Atata;
+using AtataUITests.PageObjects;
+using NUnit.Framework;
+using System;
+
+namespace AtataUITests.Tests
+{
+    public sealed class TableTestsw : UITestFixture
+    {
+        [Test]
+        public void TableColumnTest()
+        {
+            Go.To<DemoQAWebTablePage>().
+        }
+    }
+}
