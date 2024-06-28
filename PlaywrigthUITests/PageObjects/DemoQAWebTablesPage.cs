7fa48e11635290057397e10538b73234e9d1e5a3
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-06-28 23:43:28 +0300

    commit test

---
+++ PlaywrigthUITests/PageObjects/DemoQAWebTablesPage.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrigthUITests.PageObjects
+{
+	internal class DemoQAWebTablesPage
+	{
+		private IPage _page;
+		private string elementsPageUrl = "https://demoqa.com/elements";
+		private string webTablesPageUrl = "https://demoqa.com/webtables";
+
+		public DemoQAWebTablesPage(IPage page)
+		{
+			_page = page;
+		}
+
+		public async Task GoToWebTablesPage()
+		{
+			await _page.GotoAsync(elementsPageUrl);
+			await _page.GetByText("Web Tables").ClickAsync();
+			await _page.WaitForURLAsync(webTablesPageUrl);
+		}
+		#region Verifications
+		public async Task<bool> CheckHeaderNameVisibility(string name)
+		{
+			return await _page.GetByRole(AriaRole.Columnheader, new() { Name = name }).IsVisibleAsync();
+		}
+
+		public async Task<bool> CheckTableCellValueExists(string value)
+		{
+			return await _page.GetByRole(AriaRole.Gridcell, new() { Name = value }).Nth(0).IsVisibleAsync();
+		}
+
+		public async Task<bool> CheckTableCellValueByRow(int row, string value)
+		{
+			var data = await _page.GetByRole(AriaRole.Row, new() { Level = 0 }).Nth(row).InnerTextAsync();
+			return data.Contains(value);
+		}
+
+		public async Task<bool> CheckTableCellValueVisible(string value)
+		{
+			var cell = await _page.WaitForSelectorAsync($"css=.rt-td >> text={value}");
+			return await cell.IsVisibleAsync();
+		}
+
+		public async Task CheckRegistrationFormIsVisible()
+		{
+			await _page.GetByText("Registration Form").IsVisibleAsync();
+		}
+
+		public async Task CheckRegistrationFormIsNotVisible()
+		{
+			await Assertions.Expect(_page.Locator("Registration Form")).Not.ToBeVisibleAsync();
+		}
+		#endregion
+
+		#region Actions
+		public async Task ClickAddButton()
+		{
+			await _page.GetByRole(AriaRole.Button, new() { Name = "Add" }).ClickAsync();
+		}		
+		
+		public async Task InputFirstNameField(string input_text)
+		{
+			await _page.GetByPlaceholder("First Name").FillAsync(input_text);
+		}
+
+		public async Task InputLastNameField(string input_text)
+		{
+			await _page.GetByPlaceholder("Last Name").FillAsync(input_text);
+		}
+
+		public async Task InputEmailField(string input_text)
+		{
+			await _page.GetByPlaceholder("name@example.com").FillAsync(input_text);
+		}
+
+		public async Task InputAgeField(string input_text)
+		{
+			await _page.GetByPlaceholder("Age").FillAsync(input_text);
+		}
+
+		public async Task InputSalaryField(string input_text)
+		{
+			await _page.GetByPlaceholder("Salary").FillAsync(input_text);
+		}
+
+		public async Task InputDepartmentField(string input_text)
+		{
+			await _page.GetByPlaceholder("Department").FillAsync(input_text);
+		}
+
+		public async Task ClickSubmitButton()
+		{
+			await _page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
+		}
+
+		public async Task<int> GetLastFilledRow()
+		{
+			var rowsinnertextcell = await _page.Locator("role=rowgroup").AllInnerTextsAsync();
+
+			return 1;
+		}
+		#endregion
+	}
+}
---
+++ PlaywrigthUITests/Tests/WebTablesTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Playwright;
+using PlaywrigthUITests.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PlaywrigthUITests.Tests
+{
+	internal class WebTablesTests : UITestFixture
+	{
+		private DemoQAWebTablesPage _webTablesPage;
+
+		[Category("pipeline")]
+		[SetUp]
+		public void SetupCheckBoxPage()
+		{
+			_webTablesPage = new DemoQAWebTablesPage(Page);
+		}
+
+		[Test]
+		[Description("Verify table column headers on the page by names")]
+		public async Task CheckHeaderColumnNames()
+		{
+			await _webTablesPage.GoToWebTablesPage();
+
+			List<string> columnNames = new List<string> { "First Name", "Last Name", "Age", "Email", "Salary" };
+
+			foreach (string name in columnNames)
+			{
+				var result = _webTablesPage.CheckHeaderNameVisibility(name);
+				Assert.That(result.Result == true, $"Header column {name} not exist in the table");
+			}
+		}
+
+		[Test]
+		[Description("Verify table cell contains expected value")]
+		public async Task CheckTableCellValue()
+		{
+			await _webTablesPage.GoToWebTablesPage();
+			
+			var expected_value = "Cierra";
+			var result = await _webTablesPage.CheckTableCellValueExists("Kierra");
+
+			Assert.That(result == true, $"Expected cell value {expected_value} is not exist in the table");
+		}
+
+		[Test]
+		[Description("Verify table row contains expected values - name, surname etc.")]
+		public async Task CheckTableRowValues()
+		{
+			await _webTablesPage.GoToWebTablesPage();
+
+			List<string> row_data = new List<string> { "Alden", "Cantrell", "45", "alden@example.com", "12000", "Compliance" };
+			
+			var row_number = 2;
+			foreach (string value in row_data)
+			{
+				var result = _webTablesPage.CheckTableCellValueByRow(row_number, value);
+				Assert.That(result.Result == true, $"Cell with value {value} not exist in table row {row_number}");
+			}
+		}
+
+		[Test]
+		[Description("Verify User can add valid data in table")]
+		public async Task CheckAddTableValidData()
+		{
+			await _webTablesPage.GoToWebTablesPage();
+
+			var name = "autotestName";
+			var last_name = "autotestLastName";
+			var age = "42";
+			var email = "autotest@test.com";
+			var salary = "13000";
+			var department = "autotestDepartment";
+
+			var cell = await _webTablesPage.GetLastFilledRow();
+			
+			await _webTablesPage.ClickAddButton();
+
+			await _webTablesPage.CheckRegistrationFormIsVisible();
+			await _webTablesPage.InputFirstNameField(name);
+			await _webTablesPage.InputLastNameField(last_name);
+			await _webTablesPage.InputAgeField(age);
+			await _webTablesPage.InputEmailField(email);
+			await _webTablesPage.InputSalaryField(salary);
+			await _webTablesPage.InputDepartmentField(department);
+			await _webTablesPage.ClickSubmitButton();
+			
+			await _webTablesPage.CheckRegistrationFormIsNotVisible();
+
+			var row_data = new List<string> { name, last_name, age, email, salary, department };
+
+			foreach (string value in row_data)
+			{
+				var result = _webTablesPage.CheckTableCellValueVisible(value);
+				Assert.That(result.Result == true, $"Cell with value {value} not exist in table");
+			}
+		}
+	}
+}
