5baa945641b9c2582b5393b95d1cf54071d88129
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-07-04 23:45:53 +0300

    DynamicPropertiesTests

--- AtataUITests/PageObjects/DynamicPropertiesPage.cs
+++ AtataUITests/PageObjects/DynamicPropertiesPage.cs
@@ -6,5 +6,14 @@ namespace AtataUITests.PageObjects
     [Url("dynamic-properties")]
     public sealed class DynamicPropertiesPage : Page<_>
     {
+        public Button<_> ColorChange { get; private set; }
+
+        [FindById("enableAfter")]
+        public Button<_> Enable5Sec { get; private set; }
+
+
+        [FindById("visibleAfter")]
+        [WaitFor(Until.MissingThenVisible, TriggerEvents.BeforeClick)]
+        public Button<_> VisibleAfter { get; private set; }
     }
 }
