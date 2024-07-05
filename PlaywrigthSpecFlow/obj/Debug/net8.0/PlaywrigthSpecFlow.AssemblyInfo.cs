28bf74975caeec06f220e9c63c2b0241f81c8f0d
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-07-05 17:30:22 +0300

    specflow addded

--- PlaywrigthSpecFlow/obj/Debug/net8.0/PlaywrigthSpecFlow.AssemblyInfo.cs
+++ PlaywrigthSpecFlow/obj/Debug/net8.0/PlaywrigthSpecFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrigthSpecFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31d97b214198fab351a3653c8f2ece7da22f2ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66cf641cf21c5abeb5c88e309b5055bbbdd07d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrigthSpecFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrigthSpecFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
