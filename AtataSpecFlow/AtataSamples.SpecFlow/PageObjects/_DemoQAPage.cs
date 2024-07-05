d86127a211802254ef22a51182051f3f0c9374fa
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-07-05 18:02:58 +0300

    save commit

--- AtataSpecFlow/AtataSamples.SpecFlow/PageObjects/_DemoQAPage.cs
+++ AtataSpecFlow/AtataSamples.SpecFlow/PageObjects/_DemoQAPage.cs
@@ -2,9 +2,10 @@
 namespace AtataSamples.SpecFlow.PageObjects
 {
     [Url("https://demoqa.com/")]
-    public abstract class _DemoQAPage<TOwner> : Page<TOwner>
-        where TOwner : _DemoQAPage<TOwner>
+    public abstract class DemoQAPage<TOwner> : Page<TOwner>
+        where TOwner : DemoQAPage<TOwner>
     {
 
     }
 }
+        
