cbcfb096cbdab31a3f4ce624298e43b493e81607
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-07-05 22:52:48 +0300

    init commit

--- PlaywrigthSpecFlow/API/Models/User.cs
+++ PlaywrigthSpecFlow/API/Models/User.cs
@@ -3,11 +3,8 @@
     public class User
     {
         public string userID { get; set; }
-
         public string username { get; set; }
-
         public List<Book> books { get; set; }
-
         public User()
         {
             books = new List<Book>();
@@ -17,21 +14,13 @@
     public class Book
     {
         public string isbn { get; set; }
-
         public string title { get; set; }
-
         public string subTitle { get; set; }
-
         public string author { get; set; }
-
         public DateTime publish_date { get; set; }
-
         public string publisher { get; set; }
-
         public int pages { get; set; }
-
         public string description { get; set; }
-
         public string website { get; set; }
     }
 }
