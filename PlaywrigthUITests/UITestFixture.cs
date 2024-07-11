c955898c095428d283452d86013fc90cc740050e
Volodymyr Demianenko <vldemyan@outlook.com>
2024-07-11 20:58:57 +0300

    remove comment

--- PlaywrigthUITests/UITestFixture.cs
+++ PlaywrigthUITests/UITestFixture.cs
@@ -15,7 +15,7 @@ namespace PlaywrigthUITests
             var playwrightDriver = await Playwright.CreateAsync();
             browser = await playwrightDriver.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
-                Headless = false // Set to false to run the browser in non-headless mode
+                Headless = false
             });
 
             var context = await browser.NewContextAsync(new BrowserNewContextOptions
