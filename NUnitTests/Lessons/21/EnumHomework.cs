588b8006f009b14e49ea3db5fccbec73f155e81e
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-07-22 22:37:07 +0300

    enum_implemented_tests

--- NUnitTests/Lessons/21/EnumHomework.cs
+++ NUnitTests/Lessons/21/EnumHomework.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Lesson21;
@@ -24,7 +25,6 @@ public class EnumHomework
         Assert.That((int)TestDataAge.Adult, Is.EqualTo(30));
     }
 
-
     [Test]
     public void SomeIntCorrespondsToSomeTestDataAgeValue()
     {
@@ -35,7 +35,6 @@ public class EnumHomework
 
         Assert.That(isAnyIntCorrespondsToTestDataAge, Is.True);
     }
-
 
     [Test]
     public void NumberOfIntCorrespondsToSomeTestDataAgeValue()
@@ -48,37 +47,37 @@ public class EnumHomework
         Assert.That(numberOfIntCorrespondToTestDataAge, Is.EqualTo(2));
     }
 
-
-    /*
-    [TestCaseSource(nameof(StringlEmentsArePresentInEnumCases))]
-    public void StringlEmentsArePresentInEnum(string[] list, int expectedNumberPresent, int expectedNumberExtra, bool areAllPresentExpected, bool areExtraElementsExpected)
+    [TestCaseSource(nameof(StringElementsArePresentInEnumCases))]
+    public void StringElmentsArePresentInEnum(string[] list, int expectedNumberPresent, int expectedNumberExtra, bool areAllPresentExpected, bool areExtraElementsExpected)
     {
         var listOfString = list.ToList();
 
-        var numberOfStringsWhichPresentInEnum = StringlEmentsArePresentInEnumCases.Length;
+        // for the first test case { "Child", "Baby", "Teenager", "Eldery", "Adult" } there are only 3 (out of 5) strings "Child", "Teenager", "Adult" are present in TestDataAge
+        // so for the first case numberOfStringsWhichPresentInEnum is 3
+        Dictionary<int, string> ageList = Enum.GetValues<TestDataAge>().ToDictionary(i => (int)i, t => t.ToString());
+        List<string> ages = ageList.Values.ToList();
 
+        var numberOfStringsWhichPresentInEnum = listOfString.Where(i => ages.Contains(i)).Count();
+        // "Baby" and "Eldery" are not present in TestDataAge, so numberOfStringsWhichAreNotPresentInEnum is 2
+        var numberOfStringsWhichAreNotPresentInEnum = listOfString.Where(i => !ages.Contains(i)).Count();
+        // for the first case not all strings are present in TestDataAge (only 3 out of 5 are present)), so expression result should be false
+        bool areAllPresent = ages.All(i => ages.Contains(listOfString.ToString()));
+		// for the first case, yes, there are 2 extra elements "Baby" and "Eldery", so result is true
+		bool areExtraElements = ages.TrueForAll(i => listOfString.Count() != ages.Count());
 
-		var numberOfStringsWhichAreNotPresentInEnum = 
-
-		var areAllPresent =
-        var areExtraElements =
-
-        Assert.That(numberOfStringsWhichPresentInEnum, Is.EqualTo(expectedNumberPresent));
+		Assert.That(numberOfStringsWhichPresentInEnum, Is.EqualTo(expectedNumberPresent));
         Assert.That(numberOfStringsWhichAreNotPresentInEnum, Is.EqualTo(expectedNumberExtra));
         Assert.That(areAllPresent, Is.EqualTo(areAllPresentExpected));
         Assert.That(areExtraElements, Is.EqualTo(areExtraElementsExpected));
-
     }
 
-
-    public static object[] StringlEmentsArePresentInEnumCases =
+	public static object[] StringElementsArePresentInEnumCases =
     {
             new object[] { new string[] { "Child", "Baby", "Teenager", "Eldery", "Adult" }, 3, 2, false, true },
-            new object[] { new string[] { "Child", "Teenager", "Adult" }, 3, 0, true, false },
-            new object[] { new string[] { "Baby", "Teenager", "Eldery" }, 1, 2, false, true },
-            new object[] { new string[] { "Adult", "Child" }, 2, 0, true, false },
-            new object[] { new string[] { "Eldery", "Baby" }, 0, 2, false, true },
-            new object[] { new string[] { }, 0, 0, true, false },
-    };
-    */
+			new object[] { new string[] { "Child", "Teenager", "Adult" }, 3, 0, true, false },
+			new object[] { new string[] { "Baby", "Teenager", "Eldery" }, 1, 2, false, true },
+			new object[] { new string[] { "Adult", "Child" }, 2, 0, true, false },
+			new object[] { new string[] { "Eldery", "Baby" }, 0, 2, false, true },
+			new object[] { new string[] { }, 0, 0, true, false },
+	};
 }
