fa4f2d37d3355cc163c0939cf71509dae1aac4c3
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-07-29 09:57:36 +0300

    homework 23

--- NUnitTests/Lessons/23/LambdaHomework.cs
+++ NUnitTests/Lessons/23/LambdaHomework.cs
@@ -1,124 +1,151 @@
 ﻿namespace NUnitTests.Lessons._23
 {
 
-    [TestFixture]
-    public class LambdaHomework
-    {
-        public int Add(int x, int y)
-        {
-            return x + y;
-        }
+	[TestFixture]
+	public class LambdaHomework
+	{
+		public int Add(int x, int y)
+		{
+			return x + y;
+		}
 
-        public int Multiply(int x, int y)
-        {
-            return x * y;
-        }
+		public int Multiply(int x, int y)
+		{
+			return x * y;
+		}
 
-        public List<int> FilterEvenNumbers(List<int> numbers)
-        {
-            var evenNumbers = new List<int>();
-            foreach (var number in numbers)
+		public List<int> FilterEvenNumbers(List<int> numbers)
+		{
+			var evenNumbers = new List<int>();
+			foreach (var number in numbers)
+			{
+				if (number % 2 == 0)
+				{
+					evenNumbers.Add(number);
+				}
+			}
+			return evenNumbers;
+		}
+
+		[Test]
+		public void Test_Add_Function()
+		{
+			var result = Add(3, 4);
+			Assert.That(result, Is.EqualTo(7));
+		}
+
+		[Test]
+		public void Test_Multiply_Function()
+		{
+			var result = Multiply(3, 4);
+			Assert.That(result, Is.EqualTo(12));
+		}
+
+		[Test]
+		public void Test_FilterEvenNumbers_Function()
+		{
+			var input = new List<int> { 1, 2, 3, 4 };
+			var expected = new List<int> { 2, 4 };
+			var result = FilterEvenNumbers(input);
+			Assert.That(result, Is.EqualTo(expected).AsCollection);
+		}
+
+
+		// TODO: Uncomment and implement lambda functions instead of regular functions
+
+		[Test]
+		public void Test_Add_Function_To_Lambda()
+		{
+			var lambda = (int x, int y) => { return x + y; };
+			int result = lambda(3, 4);
+			Assert.That(result, Is.EqualTo(7));
+		}
+
+		[Test]
+		public void Test_Multiply_Function_To_Lambda()
+		{
+			var lambda = (int x, int y) => { return x * y; };
+			var result = lambda(3, 4);
+			Assert.That(result, Is.EqualTo(12));
+		}
+
+		[Test]
+		public void Test_FilterEvenNumbers_Function_To_Lambda()
+		{
+			var lambda = (List<int> numbers) => numbers.Where(i => i % 2 == 0).ToList();
+
+			var input = new List<int> { 1, 2, 3, 4 };
+			var expected = new List<int> { 2, 4 };
+			var result = lambda(input);
+			Assert.That(result, Is.EqualTo(expected).AsCollection);
+		}
+
+		[Test]
+		public void Test_Where_Lambda()
+		{
+			var myList = new List<string> { "one", "two", "three", "four" };
+			var filterredList = myList.Where(e => e.Contains('t'));
+			Assert.That(filterredList.Count, Is.EqualTo(2));
+		}
+		// TODO: Uncomment and implement without lambda functions
+
+		[Test]
+		public void Test_Where_NoLambda()
+		{
+			var myList = new List<string> { "one", "two", "three", "four" };
+			var filterredList = new List<string>();
+
+			// Implementation with 'Contains':
+			/*foreach (string s in myList)
             {
-                if (number % 2 == 0)
+                if (s.Contains('t'))
                 {
-                    evenNumbers.Add(number);
-                }
-            }
-            return evenNumbers;
-        }
+                    filterredList.Add(s);
+				}
+			}*/
 
-        [Test]
-        public void Test_Add_Function()
-        {
-            var result = Add(3, 4);
-            Assert.That(result, Is.EqualTo(7));
-        }
+			//Impentation with loop:
+			foreach (string s in myList)
+			{
+				for (int i = 0; i < s.Length - 1; i++)
+				{
+					if (s[i] == 't')
+					{
+						filterredList.Add(s);
+						break;
+					}
+				}
+			}
 
-        [Test]
-        public void Test_Multiply_Function()
-        {
-            var result = Multiply(3, 4);
-            Assert.That(result, Is.EqualTo(12));
-        }
+			Assert.That(filterredList.Count, Is.EqualTo(2));
+		}
 
-        [Test]
-        public void Test_FilterEvenNumbers_Function()
-        {
-            var input = new List<int> { 1, 2, 3, 4 };
-            var expected = new List<int> { 2, 4 };
-            var result = FilterEvenNumbers(input);
-            Assert.That(result, Is.EqualTo(expected).AsCollection);
-        }
+		[Test]
+		public void Test_All_Lambda()
+		{
+			var myList = new List<string> { "one", "two", "three", "four" };
+			var notEmpty = myList.All(e => e.Length > 0);
+			Assert.That(notEmpty, Is.True);
+		}
+		// TODO: Uncomment and implement without lambda functions
 
+		[Test]
+		public void Test_All_NoLambda()
+		{
+			var myList = new List<string> { "one", "two", "three", "four" };
+			bool result = true;
+			
+			foreach (string s in myList)
+			{
+				if (s == "")
+				{
+					result = false;
+					break;
+				}
+			}
 
-        // TODO: Uncomment and implement lambda functions instead of regular functions
-        /*
-        [Test]
-        public void Test_Add_Function_To_Lambda()
-        {
-            var lambda = 
-            var result = lambda(3, 4);
-            Assert.That(result, Is.EqualTo(7));
-        }
-
-        [Test]
-        public void Test_Multiply_Function_To_Lambda()
-        {
-            var lambda = 
-            var result = lambda(3, 4);
-            Assert.That(result, Is.EqualTo(12));
-        }
-
-        [Test]
-        public void Test_FilterEvenNumbers_Function_To_Lambda()
-        {
-            var lambda =
-            var input = new List<int> { 1, 2, 3, 4 };
-            var expected = new List<int> { 2, 4 };
-            var result = lambda(input);
-            Assert.That(result, Is.EqualTo(expected).AsCollection);
-        }
-        */
-        [Test]
-        public void Test_Where_Lambda()
-        {
-            var myList = new List<string> { "one", "two", "three", "four" };
-            var filterredList = myList.Where(e => e.Contains('t'));
-            Assert.That(filterredList.Count, Is.EqualTo(2));
-        }
-        // TODO: Uncomment and implement without lambda functions
-        /*
-        [Test]
-        public void Test_Where_NoLambda()
-        {
-            var myList = new List<string> { "one", "two", "three", "four" };
-            var filterredList = new List<string>();
-
-            ...
-
-            Assert.That(filterredList.Count, Is.EqualTo(2));
-        }
-        */
-        [Test]
-        public void Test_All_Lambda()
-        {
-            var myList = new List<string> { "one", "two", "three", "four" };
-            var notEmpty = myList.All(e => e.Length > 0);
-            Assert.That(notEmpty, Is.True);
-        }
-        // TODO: Uncomment and implement without lambda functions
-        /*
-        [Test]
-        public void Test_All_NoLambda()
-        {
-            var myList = new List<string> { "one", "two", "three", "four" };
-            bool result
-
-            ...
-
-            Assert.That(result, Is.True);
-        }
-        */
-    }
+			Assert.That(result, Is.True);
+		}
+	}
 
 }
