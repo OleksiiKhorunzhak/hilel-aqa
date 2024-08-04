038c98d076841fdf1412155ce4e468334c5da9c8
andriimyk <Andrii.Mykytenko@TX-N-0780>
2024-08-04 16:08:28 +0300

    Added trace viewer

--- PlaywrigthSpecFlow/Bindings/UITestFixture.cs
+++ PlaywrigthSpecFlow/Bindings/UITestFixture.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Playwright;
+using System;
 using TechTalk.SpecFlow;
 
 namespace PlaywrigthSpecFlow.Bindings
@@ -10,6 +11,7 @@ namespace PlaywrigthSpecFlow.Bindings
     {
         public static IPage? Page { get; private set; }
         private static IBrowser? browser;
+        private static IBrowserContext context;
 
         //[SetUp]
         [BeforeFeature(Order = 1)]
@@ -21,22 +23,42 @@ namespace PlaywrigthSpecFlow.Bindings
                 Headless = false // Set to false to run the browser in non-headless mode
             });
 
-            var context = await browser.NewContextAsync(new BrowserNewContextOptions
+            context = await browser.NewContextAsync(new BrowserNewContextOptions
             {
                 ViewportSize = new ViewportSize
                 {
                     Width = 1920,  // Set the width to a common fullscreen width
                     Height = 1080 // Set the height to a common fullscreen height
                 }
-            });
+			});
 
-            Page = await context.NewPageAsync();
+			await context.Tracing.StartAsync(new()
+			{
+				Title = $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}",
+				Screenshots = true,
+				Snapshots = true,
+				Sources = true
+			});
+
+			Page = await context.NewPageAsync();
         }
 
         [AfterFeature]
         public static async Task Teardown()
         {
-            await Page.CloseAsync();
+			var failed = TestContext.CurrentContext.Result.Outcome == NUnit.Framework.Interfaces.ResultState.Error
+						|| TestContext.CurrentContext.Result.Outcome == NUnit.Framework.Interfaces.ResultState.Failure;
+
+			await context.Tracing.StopAsync(new()
+			{
+				Path = failed ? Path.Combine(
+					TestContext.CurrentContext.WorkDirectory,
+					"playwright-traces",
+					$"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
+				) : null,
+			});
+
+			await Page.CloseAsync();
             await browser.CloseAsync();
         }
     }
