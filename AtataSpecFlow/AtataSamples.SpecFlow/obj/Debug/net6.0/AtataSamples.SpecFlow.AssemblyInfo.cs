52ee2649bee0ee44bc663c8fc22d3acb2792ae7f
Volodymyr Demianenko <vldemyan@outlook.com>
2024-08-10 10:57:33 +0300

    fix after merge

--- AtataSpecFlow/AtataSamples.SpecFlow/obj/Debug/net6.0/AtataSamples.SpecFlow.AssemblyInfo.cs
+++ AtataSpecFlow/AtataSamples.SpecFlow/obj/Debug/net6.0/AtataSamples.SpecFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtataSamples.SpecFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabc4ac33b81e5e773e952be04c598f1b15e6339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+561b752593938a31576ec740e370d387b76c48d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtataSamples.SpecFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtataSamples.SpecFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NUnitTests/Lessons/Lesson3Logic.cs
+++ NUnitTests/Lessons/Lesson3Logic.cs
@@ -4,12 +4,12 @@
     public class Lesson3Logic
     {
         #region[TestSetup]
-        public int Speed { get; private set; }
-        public int Acceleration { get; private set; }
-        public string? Alert { get; private set; }
-        public bool IsDecelerationChargeActive { get; private set; }
-        public int Deceleration { get; private set; }
-        public int DecelerationCharge { get; private set; }
+        public int Speed { get; set; }
+        public int Acceleration { get; set; }
+        public string? Alert { get; set; }
+        public bool IsDecelerationChargeActive { get; set; }
+        public int Deceleration { get; set; }
+        public int DecelerationCharge { get; set; }
         public int Charge { get; set; }
 
         //Mock values - change at will
