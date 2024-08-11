3dce13b7e1c77633567e2bfe9e242c9dad9caafd
achekhrii <achekhrii@vector-software.com>
2024-08-11 19:46:30 +0300

    TextBox Tests added to test cases

---
+++ AtataUITests/PageObjects/DemoQATextBoxPage.cs
@@ -0,0 +1,48 @@
+﻿using Atata;
+using System.Reflection.Emit;
+using _ = AtataUITests.PageObjects.DemoQATextBoxPage;
+
+namespace AtataUITests.PageObjects
+{
+    public sealed class DemoQATextBoxPage : DemoQAPage<_>
+    {
+        [FindByPlaceholder("Full Name")]
+        public TextInput<_> FullName { get; set; }
+
+        [FindById("userName-label")]
+        public Label<_> FullNameLabel { get; set; }
+
+        [FindById("submit")]
+        public Button<_> Submit { get; set; }
+
+        [FindById("name")]
+        public Text<_> NameOutput { get; set; }
+
+        [FindById("userEmail-label")]
+        public Label<_> EmailLabel { get; private set; }
+
+        [FindByPlaceholder("name@example.com")]
+        public EmailInput<_> Email { get; set; }
+
+        [FindById("email")]
+        public Text<_> EmailOutput { get; set; }
+
+        [FindById("currentAddress-label")]
+        public Label<_> CurrentAddressLabel { get; set; }
+
+        [FindByPlaceholder("Current Address")]
+        public TextArea<_> CurrentAddress { get; set; }
+
+        [FindByXPath("//*[@id=\"currentAddress\" and @class=\"mb-1\"]")]
+        public Text<_> CurrentAddressOutput { get; set; }
+
+        [FindById("permanentAddress-label")]
+        public Label<_> PermanentAddressLabel { get; set; }
+
+        [FindById("permanentAddress")]
+        public TextArea<_> PermanentAddress { get; set; }
+
+        [FindByXPath("//*[@id=\"permanentAddress\" and @class=\"mb-1\"]")]
+        public Text<_> PermanentAddressOutput { get; set; }
+    }
+}
--- AtataUITests/Tests/TextBoxTests.cs
+++ AtataUITests/Tests/TextBoxTests.cs
@@ -1,4 +1,5 @@
 ﻿using Atata;
+using AtataUITests.PageObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,57 @@ namespace AtataUITests.Tests
         //Test Case 4: Clear text in the "Full Name" field, press 'Submit', after submitting area should be empty
         //Test Case 5: "Full Name" field accepts letters, numbers, and special characters
 
+        [Test]
+        public void VerifyFullNameLabel()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    FullNameLabel.Should.BeVisible();
+
+        }
+
+        [Test]
+        public void VerifyFullNameFieldInput()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    FullName.Should.BeVisible();
+
+        }
+
+        [Test]
+        public void VerifyFullNameSubmitted()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    FullName.Set("John Doe").
+                        ScrollDown().Submit.Click().
+                            NameOutput.Should.Contain("Name:John Doe");
+
+        }
+
+        [Test]
+        public void VerifyFullNameCleared()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    FullName.Set("John Doe").
+                        ScrollDown().Submit.Click().
+                     FullName.Clear().
+                        ScrollDown().Submit.Click().
+                        NameOutput.Should.Not.BeVisible();
+
+        }
+
+        [Test]
+        public void VerifyFullNameAcceptAnyStringValue()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    FullName.Set("John Doe 12 .@!#%&'*+/=?^_`").
+                        ScrollDown().Submit.Click().
+                        NameOutput.Should.Contain("John Doe 12 .@!#%&'*+/=?^_`");
+        }
 
         //    "Email" field
         //Test Case 1: Field title "Email" should be visible on the page
@@ -24,6 +76,58 @@ namespace AtataUITests.Tests
         //Test Case 3: Enter valid format ("johndoe@gmail.com") in the "Email" field, press 'Submit', text "Email:johndoe@gmail.com" should be visible in the after submitting area
         //Test Case 4: Enter invalid fomat ("johndoegmail.com" or "johndoe@gmailcom") in the "Email" field, press 'Submit', the "email" field should be highlighted with red, and after submitting area should be empty
         //Test Case 5: Clear text in the "Email" field, press 'Submit', after submitting area should be empty
+
+        [Test]
+        public void VerifyEmailLabel()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                EmailLabel.Should.BeVisible();
+        }
+
+        [Test]
+        public void VerifyEmailFieldInput()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                Email.Should.BeVisible();
+        }
+
+        [Test]
+        public void VerifyValidEmailSubmitted()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    Email.Set("johndoe@gmail.com").
+                        ScrollDown().Submit.Click().
+                        EmailOutput.Should.Contain("Email:johndoe@gmail.com");
+
+        }
+
+        [Test]
+
+        public void VerifyInvalidEmailNotSubmitted()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    Email.Set("johndoe@com").
+                        ScrollDown().Submit.Click().
+                        EmailOutput.Should.Not.BeVisible();
+
+        }
+
+        [Test]
+        public void VerifyEmailFieldCleared()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    Email.Set("johndoe@gmail.com").
+                        ScrollDown().Submit.Click().
+                     Email.Clear().
+                        ScrollDown().Submit.Click().
+                        EmailOutput.Should.Not.BeVisible();
+
+        }
 
         //    "Current Address" field
         //Test Case 1: Field title "Current Address" should be visible on the page
@@ -33,11 +137,86 @@ namespace AtataUITests.Tests
         //Test Case 5: The "Current Address" field can be expanded 
 
 
+        [Test]
+        public void VerifyCurrentAddressLabel()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                CurrentAddressLabel.Should.BeVisible();
+        }
+
+        [Test]
+        public void VerifyCurrentAddressFieldInput()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                CurrentAddress.Should.BeVisible();
+        }
+
+        [Test]
+        public void VerifyCurrentAddressSubmitted()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    CurrentAddress.Set("123 Main Street, Anytown, CA 12345").
+                        ScrollDown().Submit.Click().
+                        CurrentAddressOutput.Should.Contain("Current Address :123 Main Street, Anytown, CA 12345");
+
+        }
+
+        [Test]
+        public void VerifyCurrentAddressCleared()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    CurrentAddress.Set("123 Main Street, Anytown, CA 12345").
+                        ScrollDown().Submit.Click().
+                        CurrentAddress.Clear().
+                            ScrollDown().Submit.Click().
+                            CurrentAddressOutput.Should.Not.BeVisible();
+
+
+        }
+        //Test Case 5: The "Current Address" field can be expanded - cannot be tested
+
         //    "Permanent Address" field
         //Test Case 1: Field title "Permanent Address" should be visible on the page
         //Test Case 2: Enter "123 Main Street, Anytown, CA 12345" in the "Permanent Address" field, press 'Submit', text "Current Address :123 Main Street, Anytown, CA 12345" should be visible in the after submitting area
         //Test Case 3: Clear text in the "Permanent Address" field, press 'Submit', after submitting area should be empty
         //Test Case 4: The "Permanent Address" field can be expanded 
 
+        [Test]
+        public void VerifyPermanentAddressLabel()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                PermanentAddressLabel.Should.BeVisible();
+        }
+
+        [Test]
+        public void VerifyPermanentAddressSubmitted()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    PermanentAddress.Set("123 Main Street, Anytown, CA 12345").
+                        Submit.Click().
+                        PermanentAddressOutput.Should.Contain("Permananet Address :123 Main Street, Anytown, CA 12345");
+
+        }
+
+        [Test]
+        public void VerifyPermanentAddressCleared()
+        {
+            Go.To<DemoQAElementsPage>().
+                TextBox.ClickAndGo().
+                    PermanentAddress.Set("123 Main Street, Anytown, CA 12345").
+                        Submit.Click().
+                        PermanentAddress.Clear().
+                        Submit.Click().
+                        PermanentAddressOutput.Should.Not.BeVisible();
+        }
+
+        //Test Case 4: The "Permanent Address" field can be expanded  - cannot be tested
     }
 }
+
