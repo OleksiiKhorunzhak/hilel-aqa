4abe1a63dcd71ad1cebaabc1e7c0445df6b55960
Olenka <pulya660@gmail.com>
2024-08-12 22:21:42 +0300

    Add new progect WebShop_SolarTechnology

---
+++ AtataUI_SolarPanelShop/SetUpFixture.cs
@@ -0,0 +1,22 @@
+﻿namespace AtataUI_SolarPanelShop
+{
+    [SetUpFixture]
+    public sealed class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            // Find information about AtataContext configuration on https://atata.io/getting-started/#configuration
+            AtataContext.GlobalConfiguration
+                .UseChrome()
+                    .WithArguments(
+                        "start-maximized",
+                        "disable-search-engine-choice-screen")
+                .UseBaseUrl("https://atata.io/")
+                .UseCulture("en-US")
+                .UseAllNUnitFeatures();
+
+            AtataContext.GlobalConfiguration.AutoSetUpDriverToUse();
+        }
+    }
+}
